a7557504be7c4ce267d087d8846d543e2d36acae
Bruno Ferrari <b.ferrari09@gmail.com>
2017-02-03 10:24:25 -0200

    added annotation enableautoconfig

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -1,11 +1,13 @@
 package com.example;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class DemoApplication {
 
 
